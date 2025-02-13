6cfd04e3db1ba319ee841ed460afa968b35e27e6
Gabriel Menezes <gpd.menezes@gmail.com>
2025-02-13 20:36:38 -0300

    Camera Rotation

---
+++ PlayroomDemo/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    private float rotationSpeed = 50f;
+
+    private void LateUpdate ()
+    {
+        float rotationInput = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(new Vector3(0, rotationInput, 0));
+    }
+}
